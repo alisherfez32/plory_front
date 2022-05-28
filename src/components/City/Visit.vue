4c14e803ec607715814f636e0f9ebaf7458a778a
Dadakhon <dadakhonabdulloev@gmail.com>
2022-05-28 20:59:47 +0300

    widghet

--- src/components/City/Visit.vue
+++ src/components/City/Visit.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="px-2 py-1.5 rounded-2xl m-1 cursor-pointer transform transition-all duration-200 ease hover:-translate-y-1 shadow-md w-72 h-80">
+    <div class="px-2 py-1.5 rounded-2xl m-1 cursor-pointer transform transition-all duration-200 ease hover:-translate-y-1 shadow-md w-72">
       <div class="w-full">
             <figure class="flex justify-center w-full">
-                <img class="max-h-50 rounded-2xl transform hover:scale-105 transition duration-700 ease-out" :src="node.get_image" alt="Course">
+                <img class="max-h-44 rounded-2xl transform hover:scale-105 transition duration-700 ease-out" :src="node.get_image" alt="Course">
             </figure>
       </div>
       <div class="flex-col">
